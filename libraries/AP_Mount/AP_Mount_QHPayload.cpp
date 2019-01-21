c31c4fddc7bcde9655e544a979189c9e22b6682a
sas <davidsastresas@gmail.com>
2019-01-21 17:06:05 +0100

    AP_Mount_QHPayload: limit cmd rate only when rc has changed. code cleanups

--- libraries/AP_Mount/AP_Mount_QHPayload.cpp
+++ libraries/AP_Mount/AP_Mount_QHPayload.cpp
@@ -20,23 +20,29 @@ void AP_Mount_QHPayload::update()
         return;
     }
 
-    PL_tracker_ctrl();
+    PL_vid_src();
     PL_set_zoom();
     AP_Mount_Alexmos::update();
 }
 
 
 // Send zoom to Sony Camera
-//
-// Set dead zone, only send if changed!!!
-//
 void AP_Mount_QHPayload::PL_set_zoom()
 {
     uint16_t rc_value = RC_Channels::rc_channel(_state._Zoom_ch-1)->get_control_in();
+
+    // check if rc has changed. If not return, no need to send to much equal commands.
+    if ( rc_value < _last_zoom_rc+5 && rc_value > _last_zoom_rc-5 ){
+        return;
+    }
+
+    // Set speed sensibility
+    _frontend._joystick_speed = ((_state._Speed_max-_state._Speed_min)*0.001*(rc_value))+_state._Speed_min;
+
+    // Visca needs 0x4000 as maximum zoom cmd for 30x zoom. More than that is digital zoom
     uint16_t zoom_visca_value = (1000-rc_value)*16.384;
 
-
-    _frontend._joystick_speed = ((_state._Speed_max-_state._Speed_min)*0.001*(rc_value))+_state._Speed_min;
+    FCB_zoom_msg outgoing_buffer;
 
     int valuea = zoom_visca_value & 15;
     int valuebZ = zoom_visca_value >> 4;
@@ -46,80 +52,43 @@ void AP_Mount_QHPayload::PL_set_zoom()
     int valuec = valuecZ & 15 ;
     int valued = valuedZ & 15 ;
 
-    _PL_FCB_zoom_buffer.Byte_5 =  valued;
-    _PL_FCB_zoom_buffer.Byte_6 =  valuec;
-    _PL_FCB_zoom_buffer.Byte_7 =  valueb;
-    _PL_FCB_zoom_buffer.Byte_8 =  valuea;
+    outgoing_buffer.Byte_5 =  valued;
+    outgoing_buffer.Byte_6 =  valuec;
+    outgoing_buffer.Byte_7 =  valueb;
+    outgoing_buffer.Byte_8 =  valuea;
 
-    PL_send_command((uint8_t *)&_PL_FCB_zoom_buffer, sizeof(_PL_FCB_zoom_buffer));
+    PL_send_command((uint8_t *)&outgoing_buffer, sizeof(outgoing_buffer));
+    _last_zoom_rc =  RC_Channels::rc_channel(_state._Zoom_ch-1)->get_control_in();
 }
 
 
 // Control tracker 
-void AP_Mount_QHPayload::PL_tracker_ctrl()
+void AP_Mount_QHPayload::PL_vid_src()
 {
     uint16_t Video_value = RC_Channels::rc_channel(_state._Video_ch-1)->get_control_in();
+    
+    // check if rc has changed. If not return, no need to send to much equal commands.
+    if ( Video_value < _last_vid_rc+20 && Video_value > _last_vid_rc-20 ){
+        return;
+    }
+    
+    Tracking_msg outgoing_buffer;
 
     if ( Video_value > 750 ){
-        _PL_Tracking_buffer.Video_Source = 0x00;
+        outgoing_buffer.Video_Source = 0x00;
     } else if ( Video_value > 500 ){
-        _PL_Tracking_buffer.Video_Source = 0x01;
+        outgoing_buffer.Video_Source = 0x01;
     } else if ( Video_value > 250 ){
-        _PL_Tracking_buffer.Video_Source = 0x02;
+        outgoing_buffer.Video_Source = 0x02;
     } else {
-        _PL_Tracking_buffer.Video_Source = 0x03;
+        outgoing_buffer.Video_Source = 0x03;
     }
 
-    _PL_Tracking_buffer.Header_1         = 0x7E;     // 1
-    _PL_Tracking_buffer.Header_2         = 0x7E;     // 2
-    _PL_Tracking_buffer.Address          = 0x44;     // 3
-    _PL_Tracking_buffer.Byte_4           = 0x00;     // 4
-    _PL_Tracking_buffer.Byte_5           = 0x00;     // 5
-    _PL_Tracking_buffer.Working_state    = 0x78;     // 6 Image setting mode, SD mode, OSD Mode
-    _PL_Tracking_buffer.ImSett_SD_OSD    = 0x00;     // 7 value of ^^^^
-    _PL_Tracking_buffer.X_Mov            = 0x00;     // 8,9 
-    _PL_Tracking_buffer.Y_Mov            = 0x00;     // 10,11 
-    _PL_Tracking_buffer.Confirm_tracking = 0x00;     // 12 
-    _PL_Tracking_buffer.Bright_adj       = 0x00;     // 13 
-    _PL_Tracking_buffer.Track_Square_size= 0x00;     // 14 
-    //_PL_Tracking_buffer.Video_Source     = 0x00;     // 15
-    _PL_Tracking_buffer.Byte_16          = 0x00;     // 16
-    _PL_Tracking_buffer.Byte_17          = 0x00;     // 17
-    _PL_Tracking_buffer.Byte_18          = 0x00;     // 18
-    _PL_Tracking_buffer.Byte_19          = 0x00;     // 19
-    _PL_Tracking_buffer.Byte_20          = 0x00;     // 20 pitching angle
-    _PL_Tracking_buffer.Byte_21          = 0x00;     // 21 pitching angle
-    _PL_Tracking_buffer.Byte_22          = 0x00;     // 22 pitching angle
-    _PL_Tracking_buffer.Byte_23          = 0x00;     // 23 pitching angle
-    _PL_Tracking_buffer.Byte_24          = 0x00;     // 24 viewing angle
-    _PL_Tracking_buffer.Byte_25          = 0x00;     // 25 viewing angle
-    _PL_Tracking_buffer.Byte_26          = 0x00;     // 26 GPS X
-    _PL_Tracking_buffer.Byte_27          = 0x00;     // 27 GPS X
-    _PL_Tracking_buffer.Byte_28          = 0x00;     // 28 GPS X
-    _PL_Tracking_buffer.Byte_29          = 0x00;     // 29 GPS X
-    _PL_Tracking_buffer.Byte_30          = 0x00;     // 30 
-    _PL_Tracking_buffer.Byte_31          = 0x00;     // 31
-    _PL_Tracking_buffer.Byte_32          = 0x00;     // 32
-    _PL_Tracking_buffer.Byte_33          = 0x00;     // 33
-    _PL_Tracking_buffer.Byte_34          = 0x00;     // 34
-    _PL_Tracking_buffer.Byte_35          = 0x00;     // 35
-    _PL_Tracking_buffer.Byte_36          = 0x00;     // 36
-    _PL_Tracking_buffer.Byte_37          = 0x00;     // 37
-    _PL_Tracking_buffer.Byte_38          = 0x00;     // 38
-    _PL_Tracking_buffer.Byte_39          = 0x00;     // 39
-    _PL_Tracking_buffer.Byte_40          = 0x00;     // 40
-    _PL_Tracking_buffer.Byte_41          = 0x00;     // 41
-    _PL_Tracking_buffer.Byte_42          = 0x00;     // 42
-    _PL_Tracking_buffer.Byte_43          = 0x00;     // 43
-    _PL_Tracking_buffer.Byte_44          = 0x00;     // 44
-    _PL_Tracking_buffer.Byte_45          = 0x00;     // 45
-    _PL_Tracking_buffer.Byte_46          = 0x00;     // 46
-    _PL_Tracking_buffer.Byte_47          = 0x00;     // 47
+    outgoing_buffer.Working_state    = 0x78;                                                   // 6 Image setting mode, SD mode, OSD Mode
+    outgoing_buffer.checksum = outgoing_buffer.Header_1+outgoing_buffer.Header_2+outgoing_buffer.Address+outgoing_buffer.Working_state+outgoing_buffer.Video_Source;  // on constructor already implemented headers and address
 
-    _PL_Tracking_buffer.checksum = _PL_Tracking_buffer.Header_1+_PL_Tracking_buffer.Header_2+_PL_Tracking_buffer.Address+_PL_Tracking_buffer.Working_state+_PL_Tracking_buffer.Video_Source;
-
-    PL_send_command((uint8_t *)&_PL_Tracking_buffer, sizeof(_PL_Tracking_buffer));
-
+    PL_send_command((uint8_t *)&outgoing_buffer, sizeof(outgoing_buffer));
+    _last_vid_rc =  RC_Channels::rc_channel(_state._Video_ch-1)->get_control_in();
 }
 
 
@@ -129,11 +98,8 @@ void AP_Mount_QHPayload::PL_write_params()
 
 }
 
+
 // Send command to QHPayload API
-//
-// Check carefully size!! size + header, address, etc
-//
-//
 void AP_Mount_QHPayload::PL_send_command(uint8_t* data, uint8_t size)
 {
     if (_PL_port->txspace() < (size)) {
--- libraries/AP_Mount/AP_Mount_QHPayload.h
+++ libraries/AP_Mount/AP_Mount_QHPayload.h
@@ -2,9 +2,10 @@
 
 #include "AP_Mount_Alexmos.h"
 
-// Debug
+// ---------------------------------Debug----------------------------------------------
 #include <GCS_MAVLink/GCS.h>
 // gcs().send_text(MAV_SEVERITY_CRITICAL, "RC VALUE: %5.3f", (double)zoom_visca_value);
+//--------------------------------------------------------------------------------------
 
 
 class AP_Mount_QHPayload : public AP_Mount_Alexmos
@@ -27,13 +28,12 @@ public:
 
 private:
 
+
     // Set zoom for Sony Camera
     void PL_set_zoom();
 
     // Update tracker commands
-    void PL_tracker_ctrl();
-
-    // Update RC readings
+    void PL_vid_src();
 
     // Serial related methods
     void PL_write_params();
@@ -42,52 +42,52 @@ private:
     void PL_read_incoming();
 
     struct PACKED Tracking_msg {
-        uint8_t Header_1;              // 1
-        uint8_t Header_2;              // 2
-        uint8_t Address;               // 3
-        uint8_t Byte_4;                // 4
-        uint8_t Byte_5;                // 5
-        uint8_t Working_state;         // 6 Image setting mode, SD mode, OSD Mode
-        uint8_t ImSett_SD_OSD;         // 7 value of ^^^^
-        int16_t X_Mov;                 // 8,9 
-        int16_t Y_Mov;                 // 10,11 
-        uint8_t Confirm_tracking;      // 12 
-        uint8_t Bright_adj;            // 13 
-        uint8_t Track_Square_size;     // 14 
-        uint8_t Video_Source;          // 15
-        uint8_t Byte_16;               // 16
-        uint8_t Byte_17;               // 17
-        uint8_t Byte_18;               // 18
-        uint8_t Byte_19;               // 19
-        uint8_t Byte_20;               // 20 pitching angle
-        uint8_t Byte_21;               // 21 pitching angle
-        uint8_t Byte_22;               // 22 pitching angle
-        uint8_t Byte_23;               // 23 pitching angle
-        uint8_t Byte_24;               // 24 viewing angle
-        uint8_t Byte_25;               // 25 viewing angle
-        uint8_t Byte_26;               // 26 GPS X
-        uint8_t Byte_27;               // 27 GPS X
-        uint8_t Byte_28;               // 28 GPS X
-        uint8_t Byte_29;               // 29 GPS X
-        uint8_t Byte_30;               // 30 
-        uint8_t Byte_31;               // 31
-        uint8_t Byte_32;               // 32
-        uint8_t Byte_33;               // 33
-        uint8_t Byte_34;               // 34
-        uint8_t Byte_35;               // 35
-        uint8_t Byte_36;               // 36
-        uint8_t Byte_37;               // 37
-        uint8_t Byte_38;               // 38
-        uint8_t Byte_39;               // 39
-        uint8_t Byte_40;               // 40
-        uint8_t Byte_41;               // 41
-        uint8_t Byte_42;               // 42
-        uint8_t Byte_43;               // 43
-        uint8_t Byte_44;               // 44
-        uint8_t Byte_45;               // 45
-        uint8_t Byte_46;               // 46
-        uint8_t Byte_47;               // 47
-        uint8_t checksum;              // 48
+        uint8_t Header_1           = 0x7E;               // 1
+        uint8_t Header_2           = 0x7E;               // 2
+        uint8_t Address            = 0x44;               // 3
+        uint8_t Byte_4             = 0x00;               // 4
+        uint8_t Byte_5             = 0x00;               // 5
+        uint8_t Working_state      = 0x00;               // 6 Image setting mode, SD mode, OSD Mode
+        uint8_t ImSett_SD_OSD      = 0x00;               // 7 value of ^^^^
+        int16_t X_Mov              = 0x00;               // 8,9 
+        int16_t Y_Mov              = 0x00;               // 10,11 
+        uint8_t Confirm_tracking   = 0x00;               // 12 
+        uint8_t Bright_adj         = 0x00;               // 13 
+        uint8_t Track_Square_size  = 0x00;               // 14 
+        uint8_t Video_Source       = 0x00;               // 15
+        uint8_t Byte_16            = 0x00;               // 16
+        uint8_t Byte_17            = 0x00;               // 17
+        uint8_t Byte_18            = 0x00;               // 18
+        uint8_t Byte_19            = 0x00;               // 19
+        uint8_t Byte_20            = 0x00;               // 20 pitching angle
+        uint8_t Byte_21            = 0x00;               // 21 pitching angle
+        uint8_t Byte_22            = 0x00;               // 22 pitching angle
+        uint8_t Byte_23            = 0x00;               // 23 pitching angle
+        uint8_t Byte_24            = 0x00;               // 24 viewing angle
+        uint8_t Byte_25            = 0x00;               // 25 viewing angle
+        uint8_t Byte_26            = 0x00;               // 26 GPS X
+        uint8_t Byte_27            = 0x00;               // 27 GPS X
+        uint8_t Byte_28            = 0x00;               // 28 GPS X
+        uint8_t Byte_29            = 0x00;               // 29 GPS X
+        uint8_t Byte_30            = 0x00;               // 30 
+        uint8_t Byte_31            = 0x00;               // 31
+        uint8_t Byte_32            = 0x00;               // 32
+        uint8_t Byte_33            = 0x00;               // 33
+        uint8_t Byte_34            = 0x00;               // 34
+        uint8_t Byte_35            = 0x00;               // 35
+        uint8_t Byte_36            = 0x00;               // 36
+        uint8_t Byte_37            = 0x00;               // 37
+        uint8_t Byte_38            = 0x00;               // 38
+        uint8_t Byte_39            = 0x00;               // 39
+        uint8_t Byte_40            = 0x00;               // 40
+        uint8_t Byte_41            = 0x00;               // 41
+        uint8_t Byte_42            = 0x00;               // 42
+        uint8_t Byte_43            = 0x00;               // 43
+        uint8_t Byte_44            = 0x00;               // 44
+        uint8_t Byte_45            = 0x00;               // 45
+        uint8_t Byte_46            = 0x00;               // 46
+        uint8_t Byte_47            = 0x00;               // 47
+        uint8_t checksum           = 0X00;                 // 48 initialize with header and address
     } _PL_Tracking_buffer;
 
     // Visca message for direct zoom control
@@ -101,11 +101,12 @@ private:
         uint8_t Byte_7;
         uint8_t Byte_8;
         uint8_t Byte_9 = 0xFF;
-    } _PL_FCB_zoom_buffer;
+    };
 
     AP_HAL::UARTDriver *_PL_port;
 
     bool _PL_initialised : 1;
 
-    uint16_t _Zoom_level;
+    uint16_t _last_zoom_rc;
+    uint16_t _last_vid_rc;
 };
